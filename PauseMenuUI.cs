05b037ad02593280d317b499e4cac8549ad6308b
Bread4321 <99772484+Bread4321@users.noreply.github.com>
2023-09-14 13:17:51 -0400

    Pause Menu UI

---
+++ PauseMenuUI.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuUI : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject inventoryUI;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void OpenInventory()
+    {
+        pauseMenuUI.SetActive(false);
+        inventoryUI.SetActive(true);
+    }
+
+    public void BackButton()
+    {
+        inventoryUI.SetActive(false);
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void OpenSettings()
+    {
+
+    }
+
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+}
