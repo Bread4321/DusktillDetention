982a9bd12b65d93031cc7eab4c92b6a233d58962
dcozad001 <99700140+dcozad001@users.noreply.github.com>
2023-09-21 12:35:56 -0400

    Camera and Doors and Collectable/Inventory

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,27 +1,91 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public CircleCollider2D collider;
     Vector2 movement;
     public float moveSpeed = 20f;
     Collider2D currentTrigger = null;
+    ArrayList inventory = new ArrayList();
+    public Camera[] cameras = new Camera[2];
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 1; i < cameras.Length; i++)
+        {
+            cameras[i].enabled = false;
+        }
     }
 
     void Update()
     {
-        if (Input.GetButtonDown("Interact"))
+        if (currentTrigger != null) 
         {
-            if (currentTrigger != null)
+            if (Input.GetButtonDown("Interact"))
             {
-                Destroy(currentTrigger.GetComponent("SpriteRenderer"));
+                if (currentTrigger.tag == "Collectable") 
+                {
+                    inventory.Add(currentTrigger.gameObject.name);
+                    Debug.Log(inventory.IndexOf(currentTrigger.gameObject.name));
+                    Destroy(currentTrigger.GetComponent("SpriteRenderer"));
+                    Destroy(currentTrigger);
+                }
+                else if (currentTrigger.tag == "UpDoor")
+                {
+                    string doorName = currentTrigger.gameObject.name;
+                    movement.x = 0;
+                    movement.y = 11;
+                    rb.position = rb.position + movement;
+                    if (doorName == "SchultzToHallway")
+                    {
+                        cameras[0].enabled = false;
+                        cameras[1].enabled = true;
+                    } else if (doorName == "StorageToHallway")
+                    {
+                        cameras[2].enabled = false;
+                        cameras[3].enabled = true;
+                    }
+                    else if (doorName == "HallwayToLambright")
+                    {
+                        cameras[1].enabled = false;
+                        cameras[4].enabled = true;
+                    }
+                }
+                else if (currentTrigger.tag == "DownDoor")
+                {
+                    string doorName = currentTrigger.gameObject.name;
+                    movement.x = 0;
+                    movement.y = -11;
+                    rb.position = rb.position + movement;
+                    if (doorName == "DoorToSchultzRoom")
+                    {
+                        cameras[1].enabled = false;
+                        cameras[0].enabled = true;
+                    }
+                    else if (doorName == "DoorToStorageRoom")
+                    {
+                        cameras[3].enabled = false;
+                        cameras[2].enabled = true;
+                    }
+                }
+            }
+            else if (currentTrigger.tag == "HallwayCollider")
+            {
+                Debug.Log("Hallway");
+                string doorName = currentTrigger.gameObject.name;
+                if (doorName == "RightToMiddle")
+                {
+                    cameras[1].enabled = false;
+                    cameras[3].enabled = true;
+                }
+                else if (doorName == "MiddleToRight")
+                {
+                    cameras[3].enabled = false;
+                    cameras[1].enabled = true;
+                }
             }
         }
         movement.x = Input.GetAxisRaw("Horizontal");
@@ -34,12 +98,13 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
-    private void OnTriggerEnter2D(Collider2D collider)
-    {
-        currentTrigger = collider;
-    }
     private void OnTriggerExit2D(Collider2D collider)
     {
         currentTrigger = null;
     }
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        currentTrigger = collider;
+    }
+    
 }
