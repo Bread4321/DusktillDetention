5cdca239d913917f6231cada76a0a82aa84444eb
Lincoln <lincolnwarren@gmail.com>
2023-10-03 21:58:14 -0400

    Added animation and combined things

---
+++ Assets/PauseMenuUI.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuUI : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject inventoryUI;
+    public GameObject gameOverScreen;
+    public PlayerMovement playermovement;
+    public ArrayList inventory = new ArrayList();
+
+
+    void Start()
+    {
+        inventory = playermovement.inventory;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void OpenInventory()
+    {
+        Debug.Log("Hello");
+        pauseMenuUI.SetActive(false);
+        inventoryUI.SetActive(true);
+
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            // Must change item folder name to "Resources"
+            Sprite s = Resources.Load<Sprite>(inventory[i].ToString());
+            GameObject.Find("slot" + i).GetComponent<Image>().sprite = s;
+            Debug.Log("Added: " + inventory[i] + " to slot" + i);
+        }
+    }
+
+    public void BackButton()
+    {
+        inventoryUI.SetActive(false);
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void OpenSettings()
+    {
+
+    }
+
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+
+    public void GameOver()
+    {
+        gameOverScreen.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void RestartGame()
+    {
+        gameOverScreen.SetActive(false);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
@@ -9,8 +10,13 @@ public class PlayerMovement : MonoBehaviour
     Vector2 movement;
     public float moveSpeed = 20f;
     Collider2D currentTrigger = null;
-    ArrayList inventory = new ArrayList();
-    public Camera[] cameras = new Camera[2];
+    public ArrayList inventory = new ArrayList();
+    public Camera[] cameras = new Camera[8];
+    public ChickenMovement chicken;
+    public Animator animator;
+    public UnityEngine.Rendering.Universal.Light2D globalLight;
+    public GameObject lambrightRoomObjects;
+    private bool physicsMovement = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,16 +28,66 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if (currentTrigger != null) 
+        if (currentTrigger != null)
         {
             if (Input.GetButtonDown("Interact"))
             {
-                if (currentTrigger.tag == "Collectable") 
+                if (currentTrigger.tag == "Collectable")
                 {
-                    inventory.Add(currentTrigger.gameObject.name);
-                    Debug.Log(inventory.IndexOf(currentTrigger.gameObject.name));
-                    Destroy(currentTrigger.GetComponent("SpriteRenderer"));
-                    Destroy(currentTrigger);
+                    if (currentTrigger.gameObject.name == "Bucket")
+                    {
+                        if (inventory.Contains("Egg"))
+                        {
+                            chicken.beginChickening();
+                            inventory.Remove("Egg");
+                        }
+                    } else if (string.Equals(currentTrigger.gameObject.name, "Flashlight"))
+                    {
+                        gameObject.transform.GetChild(0).gameObject.SetActive(true);
+                        inventory.Add(currentTrigger.gameObject.name);
+                        Destroy(currentTrigger.GetComponent("SpriteRenderer"));
+                        if (currentTrigger.GetComponent("BoxCollider2D") != null)
+                        {
+                            Destroy(currentTrigger.GetComponent("BoxCollider2D"));
+                        }
+                        Destroy(currentTrigger);
+                    } else if (currentTrigger.gameObject.name == "Water")
+                    {
+                        if (inventory.Contains("Bucket"))
+                        {
+                            inventory.Remove("Bucket");
+                            inventory.Add("BucketWithWater");
+                            Destroy(currentTrigger.GetComponent("SpriteRenderer"));
+                            if (currentTrigger.GetComponent("BoxCollider2D") != null)
+                            {
+                                Destroy(currentTrigger.GetComponent("BoxCollider2D"));
+                            }
+                            Destroy(currentTrigger);
+                        }
+                    }
+                    else if (currentTrigger.gameObject.name == "Fire")
+                    {
+                        if (inventory.Contains("BucketWithWater"))
+                        {
+                            inventory.Remove("BucketWithWater");
+                            Destroy(currentTrigger.GetComponent("SpriteRenderer"));
+                            if (currentTrigger.GetComponent("BoxCollider2D") != null)
+                            {
+                                Destroy(currentTrigger.GetComponent("BoxCollider2D"));
+                            }
+                            Destroy(currentTrigger);
+                        }
+                    }
+                    else
+                    {
+                        inventory.Add(currentTrigger.gameObject.name);
+                        Destroy(currentTrigger.GetComponent("SpriteRenderer"));
+                        if (currentTrigger.GetComponent("BoxCollider2D") != null)
+                        {
+                            Destroy(currentTrigger.GetComponent("BoxCollider2D"));
+                        }
+                        Destroy(currentTrigger);
+                    }
                 }
                 else if (currentTrigger.tag == "UpDoor")
                 {
@@ -52,6 +108,24 @@ public class PlayerMovement : MonoBehaviour
                     {
                         cameras[1].enabled = false;
                         cameras[4].enabled = true;
+                        physicsMovement = true;
+                        rb.freezeRotation = false;
+                        lambrightRoomObjects.SetActive(true);
+                    }
+                    else if (doorName == "EggletonToHallway")
+                    {
+                        cameras[7].enabled = false;
+                        cameras[5].enabled = true;
+                    }
+                    else if (doorName == "HallwayToBarnes")
+                    {
+                        cameras[3].enabled = false;
+                        cameras[6].enabled = true;
+                    }
+                    else if (doorName == "HallwayToRivero")
+                    {
+                        cameras[5].enabled = false;
+                        cameras[8].enabled = true;
                     }
                 }
                 else if (currentTrigger.tag == "DownDoor")
@@ -70,11 +144,39 @@ public class PlayerMovement : MonoBehaviour
                         cameras[3].enabled = false;
                         cameras[2].enabled = true;
                     }
+                    else if (doorName == "LambrightToHallway")
+                    {
+                        cameras[4].enabled = false;
+                        cameras[1].enabled = true;
+                        physicsMovement = false;
+                        rb.freezeRotation = true;
+                        transform.rotation = Quaternion.identity;
+                        lambrightRoomObjects.SetActive(false);
+                    }
+                    else if (doorName == "DoorToEggletonRoom")
+                    {
+                        cameras[5].enabled = false;
+                        cameras[7].enabled = true;
+                    }
+                    else if (doorName == "BarnesToHallway")
+                    {
+                        cameras[6].enabled = false;
+                        cameras[3].enabled = true;
+                    }
+                    else if (doorName == "RiveroToHallway")
+                    {
+                        cameras[8].enabled = false;
+                        cameras[5].enabled = true;
+                    }
+                } else if (string.Equals(currentTrigger.gameObject.name, "BreakerTrigger"))
+                {
+                    gameObject.transform.GetChild(0).gameObject.SetActive(false);
+                    globalLight.intensity = 1;
+
                 }
             }
             else if (currentTrigger.tag == "HallwayCollider")
             {
-                Debug.Log("Hallway");
                 string doorName = currentTrigger.gameObject.name;
                 if (doorName == "RightToMiddle")
                 {
@@ -86,15 +188,43 @@ public class PlayerMovement : MonoBehaviour
                     cameras[3].enabled = false;
                     cameras[1].enabled = true;
                 }
+                else if (doorName == "MiddleToLeft")
+                {
+                    cameras[3].enabled = false;
+                    cameras[5].enabled = true;
+                }
+                else if (doorName == "LeftToMiddle")
+                {
+                    cameras[5].enabled = false;
+                    cameras[3].enabled = true;
+                }
+            }
+            else if (currentTrigger.tag == "Teacher")
+            {
+                Debug.Log("You Died");
             }
         }
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("X", movement.x);
+        animator.SetFloat("Y", movement.y);
+
+        if (chicken.giveBucket)
+        {
+            chicken.giveBucket = false;
+            inventory.Add("Bucket");
+        }
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (physicsMovement)
+        {
+            rb.AddForce(movement, ForceMode2D.Impulse);
+        } else
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
 
     }
 
@@ -106,5 +236,5 @@ public class PlayerMovement : MonoBehaviour
     {
         currentTrigger = collider;
     }
-    
+
 }
